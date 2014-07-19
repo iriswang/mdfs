b6a9bcb5091e0cbc111d41d5e92e6413381efe1b
Yuxin Zhu <zhuyxn@gmail.com>
2014-07-19 12:21:06 -0700

    adding initial html files

--- server/server.py
+++ server/server.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__, static_url_path='')
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.debug = True
