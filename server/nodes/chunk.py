ef40a746a767b7a46777ec4fea4eda0aefc65355
Sharad Vikram <sharad.vikram@gmail.com>
2014-07-19 13:36:05 -0700

    working fb chunk posting

--- server/nodes/chunk.py
+++ server/nodes/chunk.py
@@ -1,3 +1,7 @@
 class Chunk:
 
-    def __init__(self,
+    def __init__(self, data, size, index, offset):
+        self.data = data
+        self.size = size
+        self.index = index
+        self.offset = offset
--- server/nodes/node.py
+++ server/nodes/node.py
@@ -1,5 +1,8 @@
 class Node:
 
+    def __init__(self, access_token):
+        self.access_token = access_token
+
     def get_chunk_data(self, chunk):
         raise NotImplementedError
 
