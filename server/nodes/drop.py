00a74942f08fb83dfc0e6d1512644160722bb6b1
Kelvin Chou <kelvin@squareup.com>
2014-07-19 14:37:56 -0700

    Actually added dropbox

---
+++ server/nodes/drop.py
@@ -0,0 +1,19 @@
+from dropbox.client import DropboxClient
+from dropbox import rest as dbrest
+import base64
+from node import Node
+import hashlib
+
+
+
+class DBNode(Node):
+
+    def __init__(self, access_token, redis):
+        Node.__init__(self, access_token, redis)
+
+    def put_chunk_data(self, chunk):
+        access_token = "JkT9Fsx17gQAAAAAAAAAs1DAbvAU3sQrP1pJ8WiTdnG7HzQHE70idZ9Ph-GBuA3s"
+        hash = hashlib.md5(chunk.data)
+        client = DropboxClient(access_token)
+        response = client.put_file('/' + hash.hexdigest(), base64.b64encode(chunk.data))
+        self.update_info('dropbox', response['path'] )
